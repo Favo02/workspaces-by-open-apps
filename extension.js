4c4dad764e939be89e53fbc10a7fca6354af7c2d
Luca Favini <luca.favini.02@gmail.com>
2023-03-05 20:20:11 +0100

    refactor(extension): code readability and rename

--- extension.js
+++ extension.js
@@ -7,153 +7,143 @@ const workspaceManager = global.workspace_manager;
 
 // initialize extension
 function init() {
-  return new WorkspaceLayout();
+  return new WorkspaceIndicator();
 }
 
 // extension workspace indicator
-class WorkspaceLayout {
+class WorkspaceIndicator {
   constructor() {}
 
+  // enable extension
   enable() {
-    this.indicators = [];
-    this.panel_button = null;
-    this.box_layout = null;
-    this.themeContext = St.ThemeContext.get_for_stage(global.stage);
+    this.workspaceIndicators = [];
+    this.panelContainer = null;
+    this.containerLayout = null;
 
-    this.add_panel_button();
+    // initialize panel container
+    this.addPanelContainer();
   }
 
+  // disable extension
   disable() {
-    this.destroy_indicators();
-    this.destroy_panel_button();
+    this.destroyWorkspaceIndicators();
+    this.destroyPanelContainer();
+
+    // disconnect signals
     workspaceManager.disconnect(this._workspaceSwitchedSIGNAL);
     workspaceManager.disconnect(this._workspaceAddedSIGNAL);
     workspaceManager.disconnect(this._workspaceRemovedSIGNAL);
     workspaceManager.disconnect(this._workspaceReorderedSIGNAL);
-
-    // disconnect this._windowAddSIGNAL this._windowRemoveSIGNAL
   }
 
-  add_panel_button() {
-    this.destroy_panel_button();
-    this.panel_button = new PanelMenu.Button(
-      0.0,
-      _("Improved Workspace Indicator")
-    );
-    this.box_layout = new St.BoxLayout();
-    this.panel_button.add_actor(this.box_layout);
+  // add workspace indicator (container of single workspace indicators) to panel
+  addPanelContainer() {
+    // remove current container
+    this.destroyPanelContainer();
 
+    this.panelContainer = new PanelMenu.Button(0, "workspace-indicator-by-open-apps");
+    this.containerLayout = new St.BoxLayout({style_class: "container"});
+    this.panelContainer.add_actor(this.containerLayout);
 
-    let [position] = "left";
+    // add container to panel
     Main.panel.addToStatusArea(
-      "improved-workspace-indicator",
-      this.panel_button,
+      "workspace-indicator-by-open-apps",
+      this.panelContainer,
       0,
-      position
+      "left"
     );
 
-    // connect to signals: switch/create/remove/reorder workspace
+    // connect signals (switch/create/remove/reorder) workspace: trigger addWorkspaceIndicators()
     this._workspaceSwitchedSIGNAL = workspaceManager.connect_after(
       "workspace-switched",
-      this.add_indicators.bind(this)
+      this.addWorkspaceIndicators.bind(this)
     );
     this._workspaceAddedSIGNAL = workspaceManager.connect_after(
       "workspace-added",
-      this.add_indicators.bind(this)
+      this.addWorkspaceIndicators.bind(this)
     );
     this._workspaceRemovedSIGNAL = workspaceManager.connect_after(
       "workspace-removed",
-      this.add_indicators.bind(this)
+      this.addWorkspaceIndicators.bind(this)
     );
     this._workspaceReorderedSIGNAL = workspaceManager.connect_after(
       "workspaces-reordered",
-      this.add_indicators.bind(this)
+      this.addWorkspaceIndicators.bind(this)
     );
 
-    this.add_indicators();
+    // add workspace indicators to container
+    this.addWorkspaceIndicators();
   }
 
-  add_indicators() {
-    this.destroy_indicators();
-    let active_index = workspaceManager.get_active_workspace_index();
+  // add a single workspace indicator for every workspace to containerLayout
+  addWorkspaceIndicators() {
+    // remove current indicators
+    this.destroyWorkspaceIndicators();
 
+    // generate indicator for each workspace
     for (let i = 0; i < workspaceManager.get_n_workspaces(); i++) {
-      let workspace = workspaceManager.get_workspace_by_index(i);
-      if (workspace !== null) {
-        let indicator = new SingleWorkspace(
-          workspace,
-          i == active_index,
-          true,
-          true
-        );
+      const workspace = workspaceManager.get_workspace_by_index(i);
 
-        this.box_layout.add_actor(indicator);
-        this.indicators.push(indicator);
-      }
+      const isActive = i == workspaceManager.get_active_workspace_index()
+
+      const indicator = new SingleWorkspaceIndicator(workspace, isActive);
+
+      this.containerLayout.add_actor(indicator);
+      this.workspaceIndicators.push(indicator);
     }
   }
 
-  destroy_indicators() {
-    let i = 0;
-    for (; i < this.indicators.length; i++) {
-      this.indicators[i].destroy();
-    }
-    this.indicators = [];
+  // remove panel container
+  destroyPanelContainer() {
+    this.destroyWorkspaceIndicators();
+
+    if (this.containerLayout !== null) this.containerLayout.destroy();
+    if (this.panelContainer !== null) this.panelContainer.destroy();
+
+    this.containerLayout = null;
+    this.panelContainer = null;
   }
 
-  destroy_panel_button() {
-    this.destroy_indicators();
-
-    if (this.box_layout !== null) this.box_layout.destroy();
-    if (this.panel_button !== null) this.panel_button.destroy();
-
-    this.box_layout = null;
-    this.panel_button = null;
+  // remove workspace indicators
+  destroyWorkspaceIndicators() {
+    for (let i = 0; i < this.workspaceIndicators.length; i++) {
+      this.workspaceIndicators[i].destroy();
+    }
+    this.workspaceIndicators = [];
   }
 }
 
 // single workspace indicator
-SingleWorkspace = GObject.registerClass(
-  class WorkspaceIndicator extends St.Button {
+const SingleWorkspaceIndicator = GObject.registerClass(
+  class SingleWorkspaceIndicator extends St.Button {
 
-    _init(workspace, active, skip_taskbar_mode, change_on_click) {
+    // initialize single indicator
+    _init(workspace, active) {
       super._init();
+
       this.active = active;
       this.workspace = workspace;
-      this.skip_taskbar_mode = skip_taskbar_mode;
+      this.layout = new St.BoxLayout({style_class: "single-workspace"})
 
-      this.createIconsContainer()
+      this.addIcons()
 
-      this.add_actor(this._iconsContainer);
+      this.add_actor(this.layout);
 
-      this.show_or_hide()
-
-      if (change_on_click) {
-        this.connect("clicked", () =>
-          this.workspace.activate(global.get_current_time())
-        );
-      }
-
-      this.show_or_hide();
+      this.connect("clicked", () =>
+        this.workspace.activate(global.get_current_time())
+      );
     }
 
-    // creates and fills the _iconsContainer component
-    createIconsContainer() {
-      // setup _iconsContainer
-      this._iconsContainer = new St.Widget({
-        layout_manager: new Clutter.FlowLayout(),
-        x_expand: true,
-        y_expand: false,
-        y_align: Clutter.ActorAlign.CENTER,
-        style_class: "single-workspace"
-      });
+    // add icons of apps in this workspace to this.layout
+    addIcons() {
 
       // get all running apps
-      let appSystem = Shell.AppSystem.get_default();
-      let runningApps = appSystem.get_running();
+      const runningApps = Shell.AppSystem.get_default().get_running();
 
-      // add icons of apps running in this workspace to _iconsContainer
-      runningApps.forEach(app => {
+      // add icons of apps running in this workspace to this.layout
+      for (let i = 0; i < runningApps.length; i++) {
+        const app = runningApps[i]
+
         if (app.is_on_workspace(this.workspace)) {
           const icon = app.get_icon()
 
@@ -163,39 +153,21 @@ SingleWorkspace = GObject.registerClass(
           });
           this._appIcon.set_gicon(icon)
     
-          this._iconsContainer.add_child(this._appIcon)
-        }
-      });
+          this.layout.add_child(this._appIcon)
+        } 
+      }
 
       // add active class if current focused workspace
       if (this.active) {
-        this._iconsContainer.add_style_class_name("active");
+        this.layout.add_style_class_name("active-workspace");
       }
     }
 
-    show_or_hide() {
-      if (this.active || this.has_user_window()) {
-        this.show();
-      } else {
-        this.hide();
-      }
-    }
-
-    has_user_window() {
-      let windows = this.workspace.list_windows();
-
-      if (!this.skip_taskbar_mode) {
-        return windows.length > 0;
-      }
-
-      return windows.some((w) => {
-        return !w.is_skip_taskbar();
-      });
-    }
-
+    // remove single workspace indicator
     destroy() {
       this.workspace.disconnect(this._windowRemovedId);
       this.workspace.disconnect(this._windowAddedId);
+      this.layout.destroy()
       super.destroy();
     }
   }
