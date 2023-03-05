3624485c61303b81c2836c9b39ce03b2340ba99d
Luca Favini <luca.favini.02@gmail.com>
2023-03-05 20:42:40 +0100

    refactor(*): remove `;`

--- extension.js
+++ extension.js
@@ -1,13 +1,13 @@
-const { Clutter, GObject, St, Shell } = imports.gi;
+const { Clutter, GObject, St, Shell } = imports.gi
 
-const Main = imports.ui.main;
-const PanelMenu = imports.ui.panelMenu;
+const Main = imports.ui.main
+const PanelMenu = imports.ui.panelMenu
 
-const workspaceManager = global.workspace_manager;
+const workspaceManager = global.workspace_manager
 
 // initialize extension
 function init() {
-  return new WorkspaceIndicator();
+  return new WorkspaceIndicator()
 }
 
 // extension workspace indicator
@@ -16,34 +16,34 @@ class WorkspaceIndicator {
 
   // enable extension
   enable() {
-    this.workspaceIndicators = [];
-    this.panelContainer = null;
-    this.containerLayout = null;
+    this.workspaceIndicators = []
+    this.panelContainer = null
+    this.containerLayout = null
 
     // initialize panel container
-    this.addPanelContainer();
+    this.addPanelContainer()
   }
 
   // disable extension
   disable() {
-    this.destroyWorkspaceIndicators();
-    this.destroyPanelContainer();
+    this.destroyWorkspaceIndicators()
+    this.destroyPanelContainer()
 
     // disconnect signals
-    workspaceManager.disconnect(this._workspaceSwitchedSIGNAL);
-    workspaceManager.disconnect(this._workspaceAddedSIGNAL);
-    workspaceManager.disconnect(this._workspaceRemovedSIGNAL);
-    workspaceManager.disconnect(this._workspaceReorderedSIGNAL);
+    workspaceManager.disconnect(this._workspaceSwitchedSIGNAL)
+    workspaceManager.disconnect(this._workspaceAddedSIGNAL)
+    workspaceManager.disconnect(this._workspaceRemovedSIGNAL)
+    workspaceManager.disconnect(this._workspaceReorderedSIGNAL)
   }
 
   // add workspace indicator (container of single workspace indicators) to panel
   addPanelContainer() {
     // remove current container
-    this.destroyPanelContainer();
+    this.destroyPanelContainer()
 
-    this.panelContainer = new PanelMenu.Button(0, "workspace-indicator-by-open-apps");
-    this.containerLayout = new St.BoxLayout({style_class: "container"});
-    this.panelContainer.add_actor(this.containerLayout);
+    this.panelContainer = new PanelMenu.Button(0, "workspace-indicator-by-open-apps")
+    this.containerLayout = new St.BoxLayout({style_class: "container"})
+    this.panelContainer.add_actor(this.containerLayout)
 
     // add container to panel
     Main.panel.addToStatusArea(
@@ -51,65 +51,65 @@ class WorkspaceIndicator {
       this.panelContainer,
       0,
       "left"
-    );
+    )
 
     // connect signals (switch/create/remove/reorder) workspace: trigger addWorkspaceIndicators()
     this._workspaceSwitchedSIGNAL = workspaceManager.connect_after(
       "workspace-switched",
       this.addWorkspaceIndicators.bind(this)
-    );
+    )
     this._workspaceAddedSIGNAL = workspaceManager.connect_after(
       "workspace-added",
       this.addWorkspaceIndicators.bind(this)
-    );
+    )
     this._workspaceRemovedSIGNAL = workspaceManager.connect_after(
       "workspace-removed",
       this.addWorkspaceIndicators.bind(this)
-    );
+    )
     this._workspaceReorderedSIGNAL = workspaceManager.connect_after(
       "workspaces-reordered",
       this.addWorkspaceIndicators.bind(this)
-    );
+    )
 
     // add workspace indicators to container
-    this.addWorkspaceIndicators();
+    this.addWorkspaceIndicators()
   }
 
   // add a single workspace indicator for every workspace to containerLayout
   addWorkspaceIndicators() {
     // remove current indicators
-    this.destroyWorkspaceIndicators();
+    this.destroyWorkspaceIndicators()
 
     // generate indicator for each workspace
     for (let i = 0; i < workspaceManager.get_n_workspaces(); i++) {
-      const workspace = workspaceManager.get_workspace_by_index(i);
+      const workspace = workspaceManager.get_workspace_by_index(i)
 
       const isActive = i == workspaceManager.get_active_workspace_index()
 
-      const indicator = new SingleWorkspaceIndicator(workspace, isActive);
+      const indicator = new SingleWorkspaceIndicator(workspace, isActive)
 
-      this.containerLayout.add_actor(indicator);
-      this.workspaceIndicators.push(indicator);
+      this.containerLayout.add_actor(indicator)
+      this.workspaceIndicators.push(indicator)
     }
   }
 
   // remove panel container
   destroyPanelContainer() {
-    this.destroyWorkspaceIndicators();
+    this.destroyWorkspaceIndicators()
 
-    if (this.containerLayout !== null) this.containerLayout.destroy();
-    if (this.panelContainer !== null) this.panelContainer.destroy();
+    if (this.containerLayout !== null) this.containerLayout.destroy()
+    if (this.panelContainer !== null) this.panelContainer.destroy()
 
-    this.containerLayout = null;
-    this.panelContainer = null;
+    this.containerLayout = null
+    this.panelContainer = null
   }
 
   // remove workspace indicators
   destroyWorkspaceIndicators() {
     for (let i = 0; i < this.workspaceIndicators.length; i++) {
-      this.workspaceIndicators[i].destroy();
+      this.workspaceIndicators[i].destroy()
     }
-    this.workspaceIndicators = [];
+    this.workspaceIndicators = []
   }
 }
 
@@ -119,26 +119,28 @@ const SingleWorkspaceIndicator = GObject.registerClass(
 
     // initialize single indicator
     _init(workspace, active) {
-      super._init();
+      super._init()
 
-      this.active = active;
-      this.workspace = workspace;
+      this.active = active
+      this.workspace = workspace
       this.layout = new St.BoxLayout({style_class: "single-workspace"})
 
       this.addIcons()
 
-      this.add_actor(this.layout);
+      this.add_actor(this.layout)
 
       this.connect("clicked", () =>
         this.workspace.activate(global.get_current_time())
-      );
+      )
+
+      
     }
 
     // add icons of apps in this workspace to this.layout
     addIcons() {
 
       // get all running apps
-      const runningApps = Shell.AppSystem.get_default().get_running();
+      const runningApps = Shell.AppSystem.get_default().get_running()
 
       // add icons of apps running in this workspace to this.layout
       for (let i = 0; i < runningApps.length; i++) {
@@ -150,7 +152,7 @@ const SingleWorkspaceIndicator = GObject.registerClass(
           this._appIcon = new St.Icon({
             y_align: Clutter.ActorAlign.CENTER,
             style_class: "app-icon"
-          });
+          })
           this._appIcon.set_gicon(icon)
     
           this.layout.add_child(this._appIcon)
@@ -159,16 +161,16 @@ const SingleWorkspaceIndicator = GObject.registerClass(
 
       // add active class if current focused workspace
       if (this.active) {
-        this.layout.add_style_class_name("active-workspace");
+        this.layout.add_style_class_name("active-workspace")
       }
     }
 
     // remove single workspace indicator
     destroy() {
-      this.workspace.disconnect(this._windowRemovedId);
-      this.workspace.disconnect(this._windowAddedId);
+      this.workspace.disconnect(this._windowRemovedId)
+      this.workspace.disconnect(this._windowAddedId)
       this.layout.destroy()
-      super.destroy();
+      super.destroy()
     }
   }
-);
+)
--- prefs.js
+++ prefs.js
@@ -1,14 +1,14 @@
-const GObject = imports.gi.GObject;
-const Gtk = imports.gi.Gtk;
+const GObject = imports.gi.GObject
+const Gtk = imports.gi.Gtk
 
 function init() {
 
 }
 
 function buildPrefsWidget() {
-	let widget = new PrefsWidget();
-	widget.show();
-	return widget;
+	let widget = new PrefsWidget()
+	widget.show()
+	return widget
 }
 
 const PrefsWidget = new GObject.Class({
@@ -17,9 +17,9 @@ const PrefsWidget = new GObject.Class({
 	Extends: Gtk.Box,
 	
 	_init: function (params) {
-		this.parent(params);
-		this.margin = 20;
-		this.set_spacing(15);
-		this.set_orientation(Gtk.Orientation.VERTICAL);
+		this.parent(params)
+		this.margin = 20
+		this.set_spacing(15)
+		this.set_orientation(Gtk.Orientation.VERTICAL)
 	}
-});
+})
