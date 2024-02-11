3b2aba8b35d1e9c62e9288a9b2983fbc1b7ff16c
Dmitriy B. <dbkkll@yandex.ru>
2024-02-11 09:32:52 +0500

    Add $BASE_PATH variable in install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+BASE_PATH="$(dirname "$(realpath "$0")")"
+
 # remove current extension version
 sudo rm -rf ~/.local/share/gnome-shell/extensions/workspaces-by-open-apps@favo02.github.com/
 
 # copy to extensions folder
-cp -r ./src ~/.local/share/gnome-shell/extensions/workspaces-by-open-apps@favo02.github.com
+cp -r "$BASE_PATH/src" ~/.local/share/gnome-shell/extensions/workspaces-by-open-apps@favo02.github.com
