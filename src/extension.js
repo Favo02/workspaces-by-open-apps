c6e330fc2545060f9c78c91324c8a44dacc59da5
Luca Favini <luca.favini.02@gmail.com>
2024-05-09 13:31:26 +0200

    refactor(css): modularize and rename css classes

--- src/extension.js
+++ src/extension.js
@@ -215,12 +215,15 @@ export default class WorkspacesByOpenApps extends Extension {
 
     const css_inline_workspace = `border-color: ${this._settings.indicator_color}`
 
-    const css_classes_workspace = [ "wboa-workspace" ]
+    const css_classes_workspace = [ "wboa-workspace", "wboa-bottom" ]
     if (is_active) css_classes_workspace.push("wboa-active")
     if (!this._settings.indicator_show_active_workspace) css_classes_workspace.push("wboa-no-indicator")
-    if (!this._settings.indicator_round_borders) css_classes_workspace.push("wboa-no-rounded")
+    if (this._settings.indicator_round_borders) css_classes_workspace.push("wboa-rounded")
 
-    return new Workspace(this._settings, workspace, windows, index, is_active, is_other_monitor, css_inline_workspace, css_classes_workspace)
+    const css_classes_panel = [ "panel-button", "wboa-panel-rounded" ]
+    if (!this._settings.indicator_round_borders) css_classes_panel.push("wboa-no-rounded")
+
+    return new Workspace(this._settings, workspace, windows, index, is_active, is_other_monitor, css_classes_panel, css_inline_workspace, css_classes_workspace)
   }
 
 }
